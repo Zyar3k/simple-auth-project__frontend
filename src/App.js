202861aa901b0075f4d77e19e79284c9480ec663
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-31 21:01:45 +0100

    Add routes pattern

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
 import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Bye, bye World!</h1>
-    </div>
+    <Routes>
+      <Route path="/" element={<h2>Home</h2>}></Route>
+      <Route path="/login" element={<h2>Login</h2>}></Route>
+      <Route path="/dashboard" element={<h2>Dashboard</h2>}></Route>
+      <Route path="/setting" element={<h2>Settings</h2>}></Route>
+    </Routes>
   );
 };
 
