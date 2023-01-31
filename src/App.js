edec0b068476380a47fa30c7852755b7984dc366
Arkadiusz Zygan <a.zygan@gmail.com>
2023-01-31 20:58:13 +0100

    Init project, packages

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Bye, bye World!</h1>
+    </div>
+  );
+};
+
+export default App;
